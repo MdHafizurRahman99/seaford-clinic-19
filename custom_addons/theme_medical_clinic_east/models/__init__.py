7de1b182ae659ed9bdde8ad9a08220a51e7e1f3b
hfvector <hfhafiz0@gmail.com>
2026-07-01 14:02:46 +0600

    feat(theme_medical_clinic_east): add ResConfigSettings model for two-factor authentication configuration

--- custom_addons/theme_medical_clinic_east/models/__init__.py
+++ custom_addons/theme_medical_clinic_east/models/__init__.py
@@ -3,3 +3,4 @@ from . import medical_clinic_profile
 from . import medical_clinic_contact_request
 from . import medical_clinic_test_page_submission
 from . import ir_module_module
+from . import res_config_settings
